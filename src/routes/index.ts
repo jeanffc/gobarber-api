a0f2a5ca38e4119e010a534f99488c0c25a9635b
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2020-06-23 22:29:28 -0300

    feat: configs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World!' }));
+routes.get('/', (req, res) => res.json({ message: 'Hello World 1!' }));
 
 export default routes;
